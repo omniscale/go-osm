6b01d50720dcf3ad41f5b903d455e0659b91270a
Oliver Tonnhofer <olt@bogosoft.com>
2018-09-17 13:43:23 +0200

    diff: refactor api

--- parser/diff/parser.go
+++ parser/diff/parser.go
@@ -2,14 +2,15 @@ package diff
 
 import (
 	"compress/gzip"
+	"context"
 	"encoding/xml"
 	"io"
 	"log"
-	"os"
 	"strconv"
 	"time"
 
 	"github.com/omniscale/go-osm/element"
+	"github.com/pkg/errors"
 )
 
 type Element struct {
@@ -24,83 +25,46 @@ type Element struct {
 // Parser is a stream based parser for OSM diff files (.osc).
 // Parsing is handled in a background goroutine.
 type Parser struct {
-	reader   io.Reader
-	elems    chan Element
-	errc     chan error
-	metadata bool
-	running  bool
-	onClose  func() error
+	reader io.Reader
+	conf   Config
 }
 
-// SetWithMetadata enables parsing of metadata
-func (p *Parser) SetWithMetadata(metadata bool) {
-	p.metadata = metadata
+type Config struct {
+	// IncludeMetadata indicates whether metadata like timestamps, versions and
+	// user names should be parsed.
+	IncludeMetadata bool
+
+	// Elements specifies the destination for parsed elements.
+	Elements chan Element
+
+	// KeepOpen specifies whether the destination channel should be keept open
+	// after Parse(). By default, the Elements channel is closed after Parse().
+	KeepOpen bool
 }
 
-// Next returns the next Element of the .osc file.
-// Returns io.EOF and an empty Element if the parser
-// reached the end of the .osc file.
-func (p *Parser) Next() (Element, error) {
-	if !p.running {
-		p.running = true
-		go parse(p.reader, p.elems, p.errc, p.metadata)
+// New returns a parser from an io.Reader
+func New(r io.Reader, conf Config) *Parser {
+	return &Parser{reader: r, conf: conf}
+}
+
+// NewGZIP returns a parser from a GZIP compressed io.Reader
+func NewGZIP(r io.Reader, conf Config) (*Parser, error) {
+	r, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
 	}
-	select {
-	case elem, ok := <-p.elems:
-		if !ok {
-			p.elems = nil
-		} else {
-			return elem, nil
-		}
-	case err, ok := <-p.errc:
-		if !ok {
-			p.errc = nil
-		} else {
-			if p.onClose != nil {
-				p.onClose()
-				p.onClose = nil
+	return New(r, conf), nil
+}
+
+func (p *Parser) Parse(ctx context.Context) error {
+	if !p.conf.KeepOpen {
+		defer func() {
+			if p.conf.Elements != nil {
+				close(p.conf.Elements)
 			}
-			return Element{}, err
-		}
+		}()
 	}
-	if p.onClose != nil {
-		err := p.onClose()
-		p.onClose = nil
-		return Element{}, err
-	}
-	return Element{}, nil
-}
-
-// NewParser returns a parser from an io.Reader
-func NewParser(r io.Reader) *Parser {
-	elems := make(chan Element)
-	errc := make(chan error)
-	return &Parser{reader: r, elems: elems, errc: errc}
-}
-
-// NewOscGzParser returns a parser from a .osc.gz file
-func NewOscGzParser(fname string) (*Parser, error) {
-	file, err := os.Open(fname)
-	if err != nil {
-		return nil, err
-	}
-
-	reader, err := gzip.NewReader(file)
-	if err != nil {
-		file.Close()
-		return nil, err
-	}
-
-	elems := make(chan Element)
-	errc := make(chan error)
-	return &Parser{reader: reader, elems: elems, errc: errc, onClose: file.Close}, nil
-}
-
-func parse(reader io.Reader, elems chan Element, errc chan error, metadata bool) {
-	defer close(elems)
-	defer close(errc)
-
-	decoder := xml.NewDecoder(reader)
+	decoder := xml.NewDecoder(p.reader)
 
 	add := false
 	mod := false
@@ -116,8 +80,7 @@ NextToken:
 	for {
 		token, err := decoder.Token()
 		if err != nil {
-			errc <- err
-			return
+			return errors.Wrap(err, "decoding next XML token")
 		}
 
 		switch tok := token.(type) {
@@ -146,7 +109,7 @@ NextToken:
 						node.Long, _ = strconv.ParseFloat(attr.Value, 64)
 					}
 				}
-				if metadata {
+				if p.conf.IncludeMetadata {
 					setElemMetadata(tok.Attr, &node.OSMElem)
 				}
 			case "way":
@@ -155,7 +118,7 @@ NextToken:
 						way.ID, _ = strconv.ParseInt(attr.Value, 10, 64)
 					}
 				}
-				if metadata {
+				if p.conf.IncludeMetadata {
 					setElemMetadata(tok.Attr, &way.OSMElem)
 				}
 			case "relation":
@@ -164,7 +127,7 @@ NextToken:
 						rel.ID, _ = strconv.ParseInt(attr.Value, 10, 64)
 					}
 				}
-				if metadata {
+				if p.conf.IncludeMetadata {
 					setElemMetadata(tok.Attr, &rel.OSMElem)
 				}
 			case "nd":
@@ -237,8 +200,8 @@ NextToken:
 				rel = &element.Relation{}
 				newElem = true
 			case "osmChange":
-				errc <- io.EOF
-				return
+				// EOF
+				return nil
 			}
 
 			if newElem {
@@ -249,10 +212,12 @@ NextToken:
 					tags = make(map[string]string)
 				}
 				newElem = false
-				elems <- e
+				p.conf.Elements <- e
 			}
 		}
 	}
+
+	return nil
 }
 
 func setElemMetadata(attrs []xml.Attr, elem *element.OSMElem) {
--- parser/diff/parser_test.go
+++ parser/diff/parser_test.go
@@ -1,40 +1,168 @@
 package diff
 
 import (
-	"io"
+	"context"
+	"os"
+	"reflect"
+	"sync"
 	"testing"
+
+	"github.com/kr/pretty"
+	"github.com/omniscale/go-osm/element"
 )
 
 func TestParse(t *testing.T) {
-	p, err := NewOscGzParser("612.osc.gz")
+	conf := Config{
+		Elements:        make(chan Element),
+		IncludeMetadata: true,
+	}
+	f, err := os.Open("612.osc.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	p, err := NewGZIP(f, conf)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	p.SetWithMetadata(true)
-
-	e, err := p.Next()
+	elements := []Element{}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for elem := range conf.Elements {
+			elements = append(elements, elem)
+		}
+		wg.Done()
+	}()
+	err = p.Parse(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if e.Add || !e.Mod || e.Del {
-		t.Error("element not parsed as modify", e)
-	}
-	if e.Node == nil || e.Node.ID != 25594547 {
-		t.Error("node not parsed correctly", e)
-	}
-	if md := e.Node.Metadata; md == nil || md.Version != 3 {
-		t.Error("metadata not parsed", md)
-	}
-	for {
-		_, err := p.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+	wg.Wait()
 
+	for _, tc := range []struct {
+		Idx  int
+		Name string
+		Want Element
+	}{
+		{
+			Name: "modified node",
+			Idx:  0,
+			Want: Element{
+				Add: false,
+				Mod: true,
+				Del: false,
+				Node: &element.Node{
+					OSMElem: element.OSMElem{
+						ID:       25594547,
+						Tags:     element.Tags{"source": "SRTM"},
+						Metadata: &element.Metadata{UserID: 462835, UserName: "G-eMapper", Version: 3, Timestamp: 1480688111, Changeset: 44115151},
+					},
+					Lat:  16.187913,
+					Long: 122.0913159,
+				},
+			},
+		},
+		{
+			Name: "deleted node",
+			Idx:  47,
+			Want: Element{
+				Add: false,
+				Mod: false,
+				Del: true,
+				Node: &element.Node{
+					OSMElem: element.OSMElem{
+						ID:       1884933281,
+						Metadata: &element.Metadata{UserID: 3315483, UserName: "8dirfriend", Version: 2, Timestamp: 1480688110, Changeset: 44115150},
+					},
+					Lat:  35.0233546,
+					Long: 132.879755,
+				},
+			},
+		},
+		{
+			Idx:  1753,
+			Name: "added node",
+			Want: Element{
+				Add: true,
+				Mod: false,
+				Del: false,
+				Node: &element.Node{
+					OSMElem: element.OSMElem{
+						ID:       4533952893,
+						Tags:     element.Tags{"amenity": "hospital", "name": "Кожно-венерологический диспансер", "name:ru": "Кожно-венерологический диспансер"},
+						Metadata: &element.Metadata{UserID: 4112953, UserName: "Sergei97", Version: 1, Timestamp: 1480688119, Changeset: 44115157},
+					},
+					Lat:  52.563681,
+					Long: 24.4658314,
+				},
+			},
+		},
+		{
+			Idx:  2267,
+			Name: "modified way",
+			Want: Element{
+				Add: false,
+				Mod: true,
+				Del: false,
+				Way: &element.Way{
+					OSMElem: element.OSMElem{
+						ID:       6863685,
+						Tags:     element.Tags{"highway": "unclassified", "maxspeed": "30", "name": "Oranjestraat", "oneway": "yes", "cycleway": "opposite"},
+						Metadata: &element.Metadata{UserID: 619707, UserName: "openMvD", Version: 6, Timestamp: 1480688106, Changeset: 44115110},
+					},
+					Refs:  []int64{44776397, 44776575, 4534010578, 44776865, 4534010576, 44780387},
+					Nodes: nil,
+				}},
+		},
+		{
+			Idx:  2563,
+			Name: "modified relation",
+			Want: Element{
+				Add: false,
+				Mod: true,
+				Del: false,
+				Rel: &element.Relation{
+					OSMElem: element.OSMElem{
+						ID:       2139646,
+						Tags:     element.Tags{"destination": "Balonne River", "name": "Condamine River", "type": "waterway", "waterway": "river", "wikidata": "Q805500", "wikipedia": "en:Condamine River"},
+						Metadata: &element.Metadata{UserID: 1185091, UserName: "nick0252", Version: 13, Timestamp: 1480688132, Changeset: 44115162},
+					},
+					Members: []element.Member{
+						{ID: 142320051, Type: 1, Role: "main_stream"},
+						{ID: 162045587, Type: 1, Role: "main_stream"},
+						{ID: 162045590, Type: 1, Role: "main_stream"},
+						{ID: 162047493, Type: 1, Role: "main_stream"},
+						{ID: 199021540, Type: 1, Role: "main_stream"},
+						{ID: 199021536, Type: 1, Role: "main_stream"},
+						{ID: 162077162, Type: 1, Role: "main_stream"},
+						{ID: 165967517, Type: 1, Role: "main_stream"},
+						{ID: 165967518, Type: 1, Role: "main_stream"},
+						{ID: 165967519, Type: 1, Role: "main_stream"},
+						{ID: 165967516, Type: 1, Role: "main_stream"},
+						{ID: 165967520, Type: 1, Role: "main_stream"},
+						{ID: 41454859, Type: 1, Role: "main_stream"},
+						{ID: 166808071, Type: 1, Role: "main_stream"},
+						{ID: 166814228, Type: 1, Role: "main_stream"},
+						{ID: 149724655, Type: 1, Role: "main_stream"},
+						{ID: 165368857, Type: 1, Role: "main_stream"},
+						{ID: 162077161, Type: 1, Role: "side_stream"},
+						{ID: 162077160, Type: 1, Role: "side_stream"},
+						{ID: 457226217, Type: 1, Role: "outer"},
+						{ID: 457226545, Type: 1, Role: "inner"},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			if !reflect.DeepEqual(elements[tc.Idx], tc.Want) {
+				pretty.Println(elements[tc.Idx])
+				t.Errorf("unexpected element, got:\n%#v\nwant:\n%#v", elements[tc.Idx], tc.Want)
+			}
+		})
+	}
 }
