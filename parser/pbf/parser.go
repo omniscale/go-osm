5a22bfc079a6b2e0b1e3d88e9786670721c37766
Oliver Tonnhofer <olt@bogosoft.com>
2018-09-17 15:58:55 +0200

    pbf: restructure files

--- parser/pbf/parser.go
+++ parser/pbf/parser.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"runtime"
 	"sync"
-	"sync/atomic"
 
 	"github.com/omniscale/go-osm"
 	"github.com/pkg/errors"
@@ -243,43 +242,4 @@ func (p *Parser) parseBlock(blob []byte) error {
 		}
 	}
 	return nil
-}
-
-// barrier is a struct to synchronize multiple goroutines.
-// Works similar to a WaitGroup. Except:
-// Calls callback function once all goroutines called doneWait().
-// doneWait() blocks until the callback returns. doneWait() does not
-// block after all goroutines were blocked once.
-type barrier struct {
-	synced     int32
-	wg         sync.WaitGroup
-	once       sync.Once
-	callbackWg sync.WaitGroup
-	callback   func()
-}
-
-func newBarrier(callback func()) *barrier {
-	s := &barrier{callback: callback}
-	s.callbackWg.Add(1)
-	return s
-}
-
-func (s *barrier) add(delta int) {
-	s.wg.Add(delta)
-}
-
-func (s *barrier) doneWait() {
-	if atomic.LoadInt32(&s.synced) == 1 {
-		return
-	}
-	s.wg.Done()
-	s.wg.Wait()
-	s.once.Do(s.call)
-	s.callbackWg.Wait()
-}
-
-func (s *barrier) call() {
-	s.callback()
-	atomic.StoreInt32(&s.synced, 1)
-	s.callbackWg.Done()
 }
